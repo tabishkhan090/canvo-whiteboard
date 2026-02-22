ad8571450c4d833692be3621bc6cecbd9ef36532
MD TABISH <tabishrazakhan92@gmail.com>
2026-02-22 14:00:23 +0530

    prisma is added

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -2,6 +2,7 @@ import { JWT_SECRET } from "@repo/backend-common/config"
 import { CreateUserSchema } from "@repo/common/types"
 import express from "express"
 import { Middleware } from "./middleware";
+import { prisma } from "@repo/db/prisma"
 
 const app = express();
 app.use(express.json());
--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,6 +1,7 @@
 import { JWT_SECRET } from "@repo/backend-common/config"
 import jwt from "jsonwebtoken"
 import {WebSocket, WebSocketServer} from "ws"
+import { prisma } from "@repo/db/prisma"
 
 const wss = new WebSocketServer({port: 8080});
 
