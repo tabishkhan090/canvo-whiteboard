99620234f3a53088d42d7ac50b49ec9b14780cf3
MD TABISH <tabishrazakhan92@gmail.com>
2026-07-08 00:43:15 +0530

    Updating Schema

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -7,7 +7,7 @@ export const CreateUserSchema = z.object({
         .toLowerCase()
         .email("Invalid email format"),
 
-    name: z
+    username: z
         .string()
         .trim()
         .min(2, "Name must be at least 2 characters")
@@ -26,16 +26,20 @@ export const CreateUserSchema = z.object({
 export type CreateUserInput = z.infer<typeof CreateUserSchema>;
 
 export const SigninSchema = z.object({
-    username: z.string()
-        .email("Invalid email format")
-        .trim(),
-    password: z.string()
-        .min(5, "Password must be at least 5 characters")
-        .max(20)
-        .regex(/[A-Z]/, "Must contain one uppercase letter")
-        .regex(/[a-z]/, "Must contain one lowercase letter")
-        .regex(/[0-9]/, "Must contain one number")
-        .regex(/[^A-Za-z0-9]/, "Must contain one special character"),
+    username: z
+        .string()
+        .trim()
+        .min(2, "Name must be at least 2 characters")
+        .max(50, "Name cannot exceed 50 characters"),
+
+    password: z
+        .string()
+        .min(4, "Password must be at least 4 characters")
+        .max(20, "Password cannot exceed 20 characters")
+        .regex(/[A-Z]/, "Must contain at least one uppercase letter")
+        .regex(/[a-z]/, "Must contain at least one lowercase letter")
+        .regex(/[0-9]/, "Must contain at least one number")
+        .regex(/[^A-Za-z0-9]/, "Must contain at least one special character"),
 })
 
 export const CreateRoomSchema = z.object({
